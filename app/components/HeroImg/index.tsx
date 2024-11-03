12a51e3e3e0446513a36f49de63618c27ab9fa8d
DanFromUkraine <ovsannikovdana91@gmail.com>
2024-11-03 19:45:21 +0200

    create cube component. Added animation for it. Mostly ended up with hero section

--- app/components/HeroImg/index.tsx
+++ app/components/HeroImg/index.tsx
@@ -4,10 +4,10 @@ import CubeInDots, { Dot } from "../utils/CubeInDots";
 
 export default function HeroImg() {
     return (
-        <section className="flex flex-col items-center ">
+        <section className="flex flex-col items-center relative">
             <Image src="/Hero.png" alt="Hero image" width={457} height={386}/>
             <WorkingOn />
-            <CubeInDots xSize="big" ySize="big"/>
+            <CubeInDots xSize="big" ySize="big" className="bottom-20 right-5"/>
         </section>
     );
 }
--- app/components/utils/CubeInDots.tsx
+++ app/components/utils/CubeInDots.tsx
@@ -1,29 +1,63 @@
+"use client";
+
 import { GridCols, GridRows } from "@/app/lib/constants/cubeSizes";
 import {
     AvailableGridColsSizes,
     AvailableGridRowsSizes,
 } from "@/app/lib/definitions";
 import clsx from "clsx";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 function Dot() {
-    return <span className="w-1 h-1 rounded-full !bg-white " />;
+    return <span className="cube w-1 h-1 rounded-full !bg-white opacity-0" />;
 }
 
 export default function CubeInDots({
     xSize,
     ySize,
+    className,
 }: {
     xSize: AvailableGridColsSizes;
     ySize: AvailableGridRowsSizes;
+    className?: string;
 }) {
     const colsObj = GridCols[xSize];
     const rowsObj = GridRows[ySize];
     const amountOfDots = colsObj.numb * rowsObj.numb;
 
-    console.log(new Array(amountOfDots).length);
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            const tl = gsap.timeline();
+            tl.from(".cube", {
+                opacity: 0,
+            }).to(".cube", {
+                opacity: 1,
+                duration: 0.3,
+                stagger: {
+                    from: "random",
+                    grid: "auto",
+                    each: 0.1,
+                    repeat: 0.1,
+                },
+            });
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
 
     return (
-        <div className={clsx("grid", colsObj.class, rowsObj.class, "gap-4")}>
+        <div
+            className={clsx(
+                className,
+                colsObj.class,
+                rowsObj.class,
+                "grid gap-4 absolute  w-fit h-fit"
+            )}
+            
+        >
             {new Array(amountOfDots).fill(null).map((_, i) => (
                 <Dot key={i} />
             ))}
